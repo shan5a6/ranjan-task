79564fdedc47373e28cc9e069129492576f59e95
root <root@DESKTOP-IOC981H.localdomain>
2020-07-16 01:05:23 +0400

    adding pipelines

--- packer/setup.sh
+++ packer/setup.sh
@@ -2,8 +2,10 @@
 yum install java-1.8.0-openjdk-devel -y
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import http://pkg.jenkins.io/redhat-stable/jenkins.io.key
-yum install jenkins* docker -y
+yum install jenkins* docker git -y
 yum-config-manager --disable jenkins
+usermod -aG docker -s /bin/bash jenkins
+sed -i 's/JENKINS_USER="jenkins"/JENKINS_USER="root"/g' /etc/sysconfig/jenkins
 systemctl enable jenkins
 systemctl restart jenkins
 systemctl enable docker
